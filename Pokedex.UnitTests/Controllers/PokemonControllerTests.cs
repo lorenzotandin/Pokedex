d59ffee31755073d4b5fd3229f7b9034562200ea
lorenzotandin <lorenzotandin@hotmail.com>
2024-11-26 18:37:47 +0100

    add controller and unit tests project

---
+++ Pokedex.UnitTests/Controllers/PokemonControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex.UnitTests.Controllers
+{
+    public class PokemonControllerTests
+    {
+        private PokemonController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _controller = new PokemonController();
+        }
+
+        [Test]
+        public async Task ShouldReturnTheGivenName()
+        {
+            var pokemonName = "pokemon name";
+
+            var result = await _controller.Get(pokemonName);
+
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+            Assert.That(okResult.Value, Is.EqualTo(pokemonName));
+        }
+    }
+}
---
+++ Pokedex/Controllers/PokemonController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pokedex.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        // GET: pokemon/{name}
+        [HttpGet("{name}")]
+        public async Task<IActionResult> Get(string name)
+        {
+            return Ok(name);
+        }
+    }
+}
